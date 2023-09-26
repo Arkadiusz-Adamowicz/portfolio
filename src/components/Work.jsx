363a4493e53ffb625432bcd5cb3fbe1b8358db7c
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2023-09-26 15:49:10 +0200

    change title

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -27,7 +27,7 @@ const Work = () => {
             >
               <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                 <span className='text-2xl font-bold text-white tracking-wider'>
-                  Redux Store App
+                  E-Commerce App
                 </span>
                 <div className='pt-8 text-center'>
                   <a
